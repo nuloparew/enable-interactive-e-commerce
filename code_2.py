9500740c38ccd9529abfb4364589dffe596f90e8
nuloparew <144057022+nuloparew@users.noreply.github.com>
2023-10-23 10:09:07 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+}
+
+}
+        console.log("Random Number: " + item);
+
+    data.forEach(item => {
+function main() {
+    });
+function generateRandomData() {
+    return data;
+    const data = [];
+    const data = generateRandomData();
+main();
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
