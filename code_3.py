09e507c8642c88614b078dc3eda79b198f2de0dd
nuloparew <144057022+nuloparew@users.noreply.github.com>
2023-11-03 21:40:50 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,27 +1,64 @@
-def generate_random_data():
+
     main()
+    data = generate_random_data()
+def generate_random_data():
     for item in data:
-def main():
-if __name__ == "__main__":
-    return data
-
-        print(f"Random Number: {item}")
-
 
 import random
     data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-        print(f"Random Number: {item}")
 def main():
 
-def generate_random_data():
 
-    for item in data:
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
     return data
 if __name__ == "__main__":
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-
+    for item in data:
 
     main()
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+
+
+def main():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def generate_random_data():
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+
+
+if __name__ == "__main__":
+    for item in data:        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+}
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+    }
+
+public class RandomDataGenerator {
+        }
+    public static void main(String[] args) {
+        int[] data = new int[10];        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        int[] data = new int[10];
+}
+    }
+        for (int item : data) {
+
+public class RandomDataGenerator {
