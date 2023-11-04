836125aee021a7828d22bd0fa6c9ef2f4ed49b7c
nuloparew <144057022+nuloparew@users.noreply.github.com>
2023-11-04 22:48:55 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,30 @@
+    return data
 
-    return data;
-    data.forEach(item => {
-function generateRandomData() {
-    for (let i = 0; i < 10; i++) {
-    });
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    for item in data:
+    data = generate_random_data()
+
+import random
+
+def generate_random_data():
+    main()
+def main():
+        print(f"Random Number: {item}")    }
+
 }
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-    const data = generateRandomData();
-}
-
-    const data = [];
-    }
-
         console.log("Random Number: " + item);
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+
+function generateRandomData() {
+}
+    });
+function main() {
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+main();
+    const data = generateRandomData();
+    return data;
