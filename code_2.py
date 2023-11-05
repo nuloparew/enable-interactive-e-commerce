ab4dbbbb291522a706e92f957ba29b514eb20e74
nuloparew <144057022+nuloparew@users.noreply.github.com>
2023-11-05 23:56:20 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,64 +1,27 @@
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-
-            System.out.println("Random Number: " + item);
-}
-public class RandomDataGenerator {
-
-        int[] data = new int[10];
-    public static void main(String[] args) {
-        }
-    }    data = generate_random_data()
-    main()
-def main():
-
         print(f"Random Number: {item}")
-
-    return data
-    for item in data:
-
-
-if __name__ == "__main__":
-def generate_random_data():
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-
-}
-        int[] data = new int[10];
-public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-        }
-if __name__ == "__main__":
-def main():
-import random
-
-
-        print(f"Random Number: {item}")
-
-    return data
     main()
     data = generate_random_data()
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():    data = [random.randint(1, 100) for _ in range(10)]
 import random
-
-if __name__ == "__main__":
-    main()
-
-def generate_random_data():
-        print(f"Random Number: {item}")
 
 
     return data
-    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
+def generate_random_data():
+if __name__ == "__main__":
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def generate_random_data():
+if __name__ == "__main__":
+
+
+import random
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    return data
 def main():
